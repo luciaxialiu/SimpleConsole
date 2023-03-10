e483788f236838edc1b64ed028c1f56c73672269
Xia Liu <xia.liu1@assaabloy.com>
2023-03-10 10:32:36 -0500

    first commit

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string Expected = "Hello World!";
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using(var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                ConsoleApp1.Program.Main(null);
+                var result = sw.ToString().Trim();
+
+                Assert.IsTrue(result.IndexOf(Expected)>-1);
+            }
+        }
+    }
+}
